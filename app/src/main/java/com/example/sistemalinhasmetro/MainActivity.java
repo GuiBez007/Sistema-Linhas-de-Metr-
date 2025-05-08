20277c79140b232fbfc94e4cf01b9f5210a9f09f
2830482311024 <guilhermebezerradejesus@gmail.com>
2025-05-08 19:53:48 -0300

    Criei tela de Splash

--- app/src/main/java/com/example/sistemalinhasmetro/MainActivity.java
+++ app/src/main/java/com/example/sistemalinhasmetro/MainActivity.java
@@ -8,6 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+//Desenvolver um App para auxiliar o Metro a coletar os dados para a pesquisa anual de origem e destino.
+//
+//Principais Requisitos Funcionais
+//RF01 - O sistema deve permitir que o entrevistador se identifique antes de utilizar o sistema;
+//RF02 - O sistema deve permitir que o entrevistador registre a resposta do usuário (origem e destino);
+//RF03 - O sistema deve registrar o nome e telefone do entrevistado e armazenar no banco de dados;
+//RF04 - O sistema deve exibir a quantidade de respostas por Origem (valor Absoluto e Percentual);
+//RF05 - O sistema deve exibir a quantidade de respostas por destino (valor Absoluto e Percentual);
+//RF06 - O sistema deve exibir a quantidade de respostar por origem e destino (valor Absoluto e Percentual);
+//RF07 - O sistema deve registrar automaticamente a localização onde os dados foram coletados;
+//RF08 - O sistema deve zerar os totais quando for solicitado;
+//RF09 - O sistema deve enviar os dados para um Servidor Central
+//RF10 - Acrescente um requisitos que julgue importante para esse App.
+//
+//Requisitos Não Funcionais
+//RNF01 - O sistema deve possuir uma interface gráfica com botões rotulados com o nome da estação e uma cor correspondente a linha;
+//
+// Linhas: cinza e vermelha
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
